13dd5d8edfce934a049c669cc119c5b84efa78f2
Maiconper <maicon.pereiramop@gmail.com>
2022-09-06 13:10:46 -0300

    exercicio realizado

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,5 +1,86 @@
-const num = //seu numero aqui
+//Crie um código que receba uma const numérica qualquer. 
+//Crie um `if` para verificar se o número 
+//guardado na const é **par**. Caso seja, imprima no console 
+//a mensagem: "o número é par".
 
-if(){
-    console.log("sua mensagem aqui");
+const num = prompt("Digite um número:")
+
+if(num % 2 === 0){
+    console.log("O número é par");
+} else {
+    console.log("O número é ímpar")
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+let bool1 = false
+let bool2 = true
+let(bool3) = true
+
+if(bool1 === bool2) {
+    alert("bool é igual a bool2")
+} else if (bool2 === bool3) {
+    alert("bool2 e bool3 são iguais")
+} else if(bool1 === bool3) {
+    alert("bool1 e bool3 são iguais")
+} else {
+    alert("não existem valores iguais")
+}
+
+
+const idade = prompt("Qual a sua idade?")
+
+if(idade >= 16 || idade ) {
+    console.log("Pode fazer título")
+} else {
+    console.log("não pode fazer título")
+}
+
+
+//Verificando média
+const media = prompt("Qual a nota?")
+if(media >= 5) {
+    console.log("Aprovada")
+} else if(media >=3) {
+    console.log("Recuperação")
+} else if(media <3) {
+    console.log("Reprovado")
+} else {
+    console.log("Sem dados")
+}
+*/
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,36 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+/*
+Escreva um código que receba três valores. Uma idade, 
+um booleano que responda se a pessoa terminou ou não o 
+ensino médio, e um booleano que responda se a pessoa 
+está cursando alguma faculdade.
 
-if(){
+Crie um `if` para cada variável, checando as seguintes 
+afirmacões:
+- Se a pessoa tem 18 anos ou mais;
+- Se a pessoa terminou o ensino médio;
+- Se a pessoa NÃO está cursando alguma faculdade;
+Crie um `if` que imprima que a afirmacão é verdadeira, e 
+um `else` para imprimir que a afirmacão não é verdadeira
+*/
 
-}else{
-	
+let idade = prompt("Digite sua Idade:")
+let temEnsMed = confirm("Possui Ensino Médio?")
+let fazFacul = confirm("Estuda em uma Instituição de Ensino Superior?")
+
+if(idade >= 18) {
+    console.log("É maior de idade!")
+} else {
+    console.log("É menor de idade!")
+}
+
+if(temEnsMed === true) {
+    console.log("Tem Ensino Médio!")
+} else {
+    console.log("Não tem Ensino Médio!")
+}
+
+if(fazFacul === true) {
+    console.log("Ensino Superior em Andamento")
+} else {
+    console.log("Não estuda em Ensino Superior")
 }
